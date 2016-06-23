973782f0dda30fa4e4923d8a28bb58b681ba7848
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 16:32:13 -0400

    Update ejemplo1.py

--- condicional_FOR/ejemplo1.py
+++ condicional_FOR/ejemplo1.py
@@ -1,1 +1,10 @@
+#El bucle for, en Python, es aquel que nos permitirá iterar sobre una variable compleja, del
+#tipo lista o tupla
+import msvcrt
+# -*- coding: utf-8 -*-
+#Por cada nombre en mi_lista, imprimir nombre
+mi_lista = ['Juan', 'Antonio', 'Pedro', 'Herminio']
+for nombre in mi_lista:
+    print mi_lista
 
+msvcrt.getch()
