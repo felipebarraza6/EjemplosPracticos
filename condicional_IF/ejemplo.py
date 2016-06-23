bd81afaeef2eb5527a602009413e29128c91ef00
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 12:24:26 -0400

    Ejemplo semaforo

---
+++ condicional_IF/ejemplo.py
@@ -0,0 +1,22 @@
+import msvcrt
+
+
+#Si semaforo esta en verde, cruzar la callen. Sino, esperar.
+semaforo = verde
+
+if semafoto == verde:
+  #Recordar que "==" es comprar, "=" es asignar
+    print 'Cruzar la calle'
+else:
+    print 'Esperar'
+
+
+
+
+
+
+
+
+
+
+msvcrt.getch()
