e56b77208db205a7a8542bcdb62a3ab171b4dcb8
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 15:47:51 -0400

    Update ejemplo1.py

--- condicional_WHILE/ejemplo1.py
+++ condicional_WHILE/ejemplo1.py
@@ -3,6 +3,10 @@ import msvcrt
 # -*- coding: utf-8 -*-
 
 #La estructura de while se repite con anio+=1 hasta cumplir la condicion y imprime los resultados
+#Podrás notar que en cada iteración, incrementamos el valor de la variable que condiciona
+#el bucle (anio). Si no lo hiciéramos, esta variable siempre sería igual a 2001 y el bucle se
+#ejecutaría de forma infinita, ya que la condición (anio <= 2012) siempre se estaría
+#cumpliendo.
 
 anio = 2001
   while anio <= 2016:
