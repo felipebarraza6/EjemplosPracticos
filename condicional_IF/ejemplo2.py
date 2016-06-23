d5d51fe94e3d5e84e2c33b3aad114c536e048bd5
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 12:26:48 -0400

    Create ejemplo2.py

---
+++ condicional_IF/ejemplo2.py
@@ -0,0 +1,13 @@
+#Ejemplo if anidados si gasto hasta 100 mil pago con efectivo, si gasto mas de 100 pero menos de 300 pago con debito pero! 
+#si gasto mas de 300 pago con credito.
+
+compra = 500
+
+if compra <= 100:
+    print 'pago en efectivo'
+elif compra > 100 and compra < 300:
+      #el "and" es un operador logico y asi decimos que se deben cumpliar ambas condiciones para 
+      #imprimir "pago con debito".
+    print 'pago con tarjeta de debito'
+else:
+    print 'Pago con tarjeta de credito'
