40b92ff11b682fb6df6b52c7fb5c07f69a86f6e1
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 16:34:53 -0400

    Create ejemplo2.py

---
+++ condicional_FOR/ejemplo2.py
@@ -0,0 +1,9 @@
+import msvcrt
+# -*- coding: utf-8 -*-
+
+#por cada color en mi_tupla imprimir color
+mi_tupla = ('rosa', 'verde', 'celeste', 'amarillo')
+for color in mi_tupla:
+    print color
+
+msvcrt.getch()
