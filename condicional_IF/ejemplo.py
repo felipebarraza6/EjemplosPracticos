bd1cf305bdb143294021401cbb06afef21662ad2
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-06-23 21:56:23 -0300

    Update ejemplo.py

--- condicional_IF/ejemplo.py
+++ condicional_IF/ejemplo.py
@@ -4,7 +4,7 @@ import msvcrt
 #Si semaforo esta en verde, cruzar la callen. Sino, esperar.
 semaforo = verde
 
-if semafoto == verde:
+if semaforo == verde:
   #Recordar que "==" es comparar, "=" es asignar
     print 'Cruzar la calle'
 else:
